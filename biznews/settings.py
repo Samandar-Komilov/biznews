d709cb08acb614803e56152fe9ea43394ad0b607
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-04-22 23:03:16 +0500

    Basic Page layout and current time functionality added

--- biznews/settings.py
+++ biznews/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'pages.context_processors.current_datetime',
             ],
         },
     },
@@ -115,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
