45fa7c1183ba573fc00d63ae06f2e025a4f1b219
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-04-22 21:38:45 +0500

    Improved admin pnel

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.action(description="Make the Post Published")
+def publish_post(modeladmin, request, queryset):
+    queryset.update(status='PB')
+
+@admin.action(description="Make the Post Draft")
+def draft_post(modeladmin, request, queryset):
+    queryset.update(status='DF')
+
+
+@admin.register(models.New)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'publish_time', 'status']
+    # list_editable = ['subtitle']
+    list_display_links = ['title', 'publish_time']
+    list_filter = ['status', 'category']
+    prepopulated_fields = {'slug':['title']}
+    date_hierarchy = 'publish_time'
+    search_fields = ['title', 'subtitle', 'body']
+    ordering = ['-publish_time']
+    actions = [publish_post, draft_post]
+    # We don't need to specify these fields explicitly
+    exclude = ['created_time', 'publish_time']
+    # readonly_fields = ["slug"]
+
+
+admin.site.register(models.Category)
+
+admin.site.register(models.Tag)
